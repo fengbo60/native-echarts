871767f39e50cf9360aa64077495413ef0cb48cb
冯博 <2385623803@qq.com>
2018-09-21 09:52:42 +0800

    test

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { WebView, View } from 'react-native';
+import { Container, Echarts } from './components'
+
+export default class App extends Component {
+  render() {
+    return (
+        
+      <Container width={this.props.width} style={{flex: 1}}>
+        <Echarts {...this.props} />
+      </Container>
+    );
+  }
+}
